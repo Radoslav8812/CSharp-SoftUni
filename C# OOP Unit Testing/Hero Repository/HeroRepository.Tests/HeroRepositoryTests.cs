2432f00725bc4bce4710e86c2e67ee96850bfa42
Radoslav Bogdanov <radimk8812@abv.bg>
2022-04-17 20:54:01 +0300

    C# Unit Test

---
+++ C# OOP Unit Testing/Hero Repository/HeroRepository.Tests/HeroRepositoryTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using NUnit.Framework;
+
+public class HeroRepositoryTests
+{
+    [Test]
+    public void CtorValidation()
+    {
+        var hero = new Hero("Spiro", 100);
+
+        Assert.AreEqual("Spiro", hero.Name);
+        Assert.AreEqual(100, hero.Level);
+        
+    }
+
+    [Test]
+    public void PropertyCreateThrowExceptionNullEmpty()
+    {
+        var repo = new HeroRepository();
+
+        Assert.Throws<ArgumentNullException>(() => repo.Create(null));
+    }
+
+    [Test]
+    public void PropertyCreateThrowExceptionExist()
+    {
+        var repo = new HeroRepository();
+        repo.Create(new Hero("Spiro", 100));
+
+        Assert.Throws<InvalidOperationException>(() => repo.Create(new Hero("Spiro", 100)));
+    }
+
+    [Test]
+    public void PropertyCreateValidation()
+    {
+        var repo = new HeroRepository();
+        repo.Create(new Hero("Spiro", 100));
+
+        Assert.AreEqual(1, repo.Heroes.Count);
+    }
+
+    [Test]
+    public void PropertyRemoveThrowExceptionNullWhiteSpace()
+    {
+        var repo = new HeroRepository();
+
+        Assert.Throws<ArgumentNullException>(() => repo.Remove(null));
+        Assert.Throws<ArgumentNullException>(() => repo.Remove(string.Empty));
+    }
+
+    [Test]
+    public void PropertyRemoveValidation()
+    {
+        var repo = new HeroRepository();
+        repo.Create(new Hero("Spiro", 100));
+        repo.Create(new Hero("Kiro", 100));
+
+        var result = repo.Remove("Spiro");
+
+        Assert.AreEqual(true, result);
+    }
+
+    [Test]
+    public void GetHighestLevelValidation()
+    {
+        var repo = new HeroRepository();
+        repo.Create(new Hero("Spiro", 80));
+        repo.Create(new Hero("Miro", 90));
+        repo.Create(new Hero("Kiro", 100));
+
+        var result = repo.GetHeroWithHighestLevel();
+
+        Assert.AreEqual(100, result.Level);
+    }
+
+    [Test]
+    public void GetHeroValidation()
+    {
+        var repo = new HeroRepository();
+        repo.Create(new Hero("Spiro", 80));
+
+        var result = repo.GetHero("Spiro");
+
+        Assert.AreEqual("Spiro", result.Name);
+    }
+}
