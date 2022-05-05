56544af652ba4d864004760ba80006444a637616
Radoslav Bogdanov <radimk8812@abv.bg>
2022-05-05 17:27:37 +0300

    C# DB

---
+++ C# DB/Funtions/Aritmetic Operations.sql
@@ -0,0 +1,5 @@
+SELECT TOP(10) DepartmentID, ManagerID FROM Employees
+SELECT TOP(10) DepartmentID * ManagerID FROM Employees
+SELECT TOP(10) CAST(DepartmentID AS DECIMAL) / ManagerID FROM Employees
+SELECT TOP(10) DepartmentID + ManagerID FROM Employees
+SELECT TOP(10) DepartmentID - ManagerID FROM Employees
