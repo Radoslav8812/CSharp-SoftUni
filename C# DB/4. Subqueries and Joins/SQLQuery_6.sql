ab42e6438ce887d0c9c6bad984cb29774e37f93e
Radoslav Bogdanov <radimk8812@abv.bg>
2022-07-01 13:42:02 +0300

    C# DB

---
+++ C# DB/4. Subqueries and Joins/SQLQuery_6.sql
@@ -0,0 +1,34 @@
+
+-- 1.	Employee Address
+SELECT TOP(5) e.EmployeeID, e.JobTitle, a.AddressID, a.AddressText
+FROM Employees AS [e]
+JOIN Addresses AS [a] ON [e].AddressID = [a].[AddressID]
+ORDER BY AddressID
+
+--2.	Addresses with Towns
+SELECT TOP(50) e.FirstName, e.LastName, t.Name, a.AddressText
+FROM Employees AS [e]
+JOIN Addresses AS [a] ON [e].AddressID = [a].[AddressID]
+JOIN Towns as [t] ON [t].TownID = [a].TownID
+ORDER BY FirstName ASC, LastName ASC
+
+--3.	Sales Employee
+SELECT e.EmployeeID, e.FirstName, e.LastName, d.Name
+FROM Employees AS [e]
+JOIN Departments as [d] ON [e].DepartmentID = [d].DepartmentID
+WHERE [d].Name IN ('Sales')
+ORDER BY EmployeeID ASC
+
+--4.	Employee Departments
+SELECT TOP(5) e.EmployeeID, e.FirstName, e.Salary, d.Name
+FROM Employees AS [e]
+JOIN Departments AS [d] ON [e].DepartmentID = [d].DepartmentID
+WHERE [e].Salary > 15000
+ORDER BY [d].DepartmentID ASC
+
+--5.	Employees Without Project
+SELECT TOP(3) e.EmployeeID, e.FirstName
+FROM Employees AS [e]
+LEFT JOIN EmployeesProjects AS [ep] ON [e].EmployeeID = [ep].EmployeeID
+WHERE [ep].EmployeeID IS NULL
+ORDER BY [e].EmployeeID ASC
