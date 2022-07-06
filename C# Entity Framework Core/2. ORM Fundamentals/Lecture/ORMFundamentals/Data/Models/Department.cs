b75f6491dfcbf044a8ede656058d0202db631010
Radoslav Bogdanov <radimk8812@abv.bg>
2022-07-06 20:22:58 +0300

    C# EF Core

---
+++ C# Entity Framework Core/2. ORM Fundamentals/Lecture/ORMFundamentals/Data/Models/Department.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ORMFundamentals.Data.Models
+{
+	public class Department
+	{
+		public Department()
+		{
+			Employees = new HashSet<Employee>();
+		}
+
+        [Key]
+		public int DepartmentID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [ForeignKey(nameof(Employee))]		
+		public int? ManagerID { get; set; }
+		public virtual Employee Employee { get; set; }
+
+        public virtual ICollection<Employee> Employees { get; set; }
+    }
+}
+
---
+++ C# Entity Framework Core/2. ORM Fundamentals/Lecture/ORMFundamentals/Data/Models/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ORMFundamentals.Data.Models
+{
+	public class Employee
+	{
+		public Employee()
+		{
+            ManagedDepartments = new HashSet<Department>();
+		}
+
+        [Key]
+		public int EmployeeID { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+		public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+        
+        [MaxLength(50)]
+        public string MiddleName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string JobTitle { get; set; }
+
+        [ForeignKey(nameof(Department))]
+        public int DepartmentID { get; set; }
+        public Department Department { get; set; }
+
+        public virtual ICollection<Department> ManagedDepartments { get; set; }
+    } 
+}
+
