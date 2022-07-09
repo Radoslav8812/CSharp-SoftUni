9e28c530110d699cb7096c104857410599e82715
Radoslav Bogdanov <radimk8812@abv.bg>
2022-07-09 13:52:31 +0300

    C# EF Core

---
+++ C# Entity Framework Core/EntityFramework/3. Employee Full Information/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using SoftUni.Data;
+
+namespace EFhomework
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var softUniContext = new SoftUniContext();
+            var result = GetEmployeesFullInformation(softUniContext);
+
+            Console.WriteLine(result);
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employees = context.Employees.Select(x => new
+            {
+                x.EmployeeId,
+                x.FirstName,
+                x.LastName,
+                x.MiddleName,
+                x.JobTitle,
+                x.Salary
+            }).OrderBy(x => x.EmployeeId).ToList();
+
+            var sb = new StringBuilder();
+
+            foreach (var person in employees)
+            {
+                sb.AppendLine($"{person.FirstName} {person.LastName} {person.MiddleName} {person.JobTitle} {person.Salary:f2}");
+            }
+
+            var result = sb.ToString().TrimEnd();
+            return result;
+        }
+    }
+}
+
