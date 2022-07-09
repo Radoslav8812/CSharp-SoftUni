5405458516aedde68297a3099fb5a198aa9744ea
Radoslav Bogdanov <radimk8812@abv.bg>
2022-07-09 12:53:40 +0300

    C# EF Core

---
+++ C# Entity Framework Core/EntityFramework/2. Data Base First/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EFhomework
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
