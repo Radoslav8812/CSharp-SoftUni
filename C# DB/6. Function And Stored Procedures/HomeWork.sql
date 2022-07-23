070a901e31d49288f6313f2954ab51d9f8630a8b
Radoslav Bogdanov <radimk8812@abv.bg>
2022-07-23 15:52:49 +0300

    C# DB

--- C# DB/6. Function And Stored Procedures/HomeWork.sql
+++ C# DB/6. Function And Stored Procedures/HomeWork.sql
@@ -26,17 +26,63 @@ AS
     WHERE [Name] LIKE @InputString + '%'
 
 EXEC usp_GetTownsStartingWith 's'
+GO
 
 --4.	Employees from Town
-GO
 CREATE OR ALTER PROCEDURE usp_GetEmployeesFromTown (@InputTown NVARCHAR(50))
 AS
+BEGIN
     SELECT FirstName,
            LastName
     FROM Employees AS e
     JOIN Addresses AS a ON e.AddressID = a.AddressID
     JOIN Towns AS t ON t.TownID = a.TownID
     WHERE t.Name = @InputTown
-
+END
 EXEC usp_GetEmployeesFromTown 'Monroe'
-    
+GO
+
+--5.	Salary Level Function
+CREATE FUNCTION ufn_GetSalaryLevel(@salary DECIMAL(18,4))
+RETURNS VARCHAR(50)
+AS
+BEGIN
+    DECLARE @SalaryLevel VARCHAR(50)
+
+    IF @salary < 30000
+    BEGIN
+        SET @SalaryLevel = 'Low'
+    END
+    IF @salary BETWEEN 30000 AND 50000
+    BEGIN
+        SET @SalaryLevel = 'Average'
+    END
+    IF @salary > 50000
+    BEGIN
+        SET @SalaryLevel = 'High'
+    END
+
+    RETURN @SalaryLevel
+END
+
+GO
+SELECT Salary,
+       [dbo].[ufn_GetSalaryLevel](Salary) AS SalaryLevel
+FROM Employees
+GO
+
+--6.	Employees by Salary Level
+CREATE PROCEDURE usp_EmployeesBySalaryLevel @SalaryLevel VARCHAR(50)
+AS
+BEGIN
+    SELECT FirstName,
+           LastName
+    FROM Employees
+    WHERE [dbo].[ufn_GetSalaryLevel] (Salary) = @SalaryLevel
+END
+
+GO
+
+EXEC [dbo].[usp_EmployeesBySalaryLevel] 'High'
+EXEC [dbo].[usp_EmployeesBySalaryLevel] 'Average'
+EXEC [dbo].[usp_EmployeesBySalaryLevel] 'Low'
