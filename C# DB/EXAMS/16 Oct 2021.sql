213797158f9a11195c776c17edf1f42bf3b2a71a
Radoslav Bogdanov <radimk8812@abv.bg>
2022-07-24 17:47:36 +0300

    C# DB

--- C# DB/EXAMS/16 Oct 2021.sql
+++ C# DB/EXAMS/16 Oct 2021.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE CigarShop
 GO
 
-
+--1
 CREATE TABLE [Sizes] (
     [Id] INT PRIMARY KEY IDENTITY,
     [Length] INT NOT NULL CHECK ([Length] BETWEEN 10 AND 25),
@@ -53,3 +53,76 @@ CREATE TABLE [ClientsCigars](
 
     PRIMARY KEY ([ClientId], [CigarId])
 )
+
+--2
+
+--      Querying
+--
+--5.    Cigars by Price
+SELECT
+    [CigarName],
+    [PriceForSingleCigar],
+    [ImageURL]
+FROM [Cigars]
+ORDER BY [PriceForSingleCigar] ASC, [CigarName] DESC
+
+--6.	Cigars by Taste
+SELECT
+	c.Id,
+	c.CigarName,
+	c.PriceForSingleCigar,
+	t.TasteType,
+	t.TasteStrength
+FROM [Cigars] AS [c]
+JOIN [Tastes] AS [t] ON [c].[TastId] = [t].[Id]
+WHERE t.[TasteType] IN ('Woody', 'Earthy')
+ORDER BY PriceForSingleCigar DESC
+
+
+--7.	Clients without Cigars
+SELECT
+    Id,
+    FirstName + ' ' + LastName AS ClientName,
+    Email
+FROM Clients
+WHERE NOT EXISTS (
+    SELECT ClientId
+    FROM ClientsCigars 
+    WHERE ClientId = Id)
+ORDER BY ClientName ASC
+
+--8.	First 5 Cigars
+SELECT TOP(5)
+    CigarName,
+    PriceForSingleCigar,
+    ImageURL
+FROM Cigars AS c
+JOIN Sizes AS sz ON c.SizeId = sz.Id
+WHERE sz.Length >= 12 AND (CigarName LIKE '%ci%' OR c.PriceForSingleCigar > 50) AND sz.RingRange > 2.55
+ORDER BY c.CigarName ASC, c.PriceForSingleCigar DESC
+
+--9.	Clients with ZIP Codes
+SELECT
+    cli.FirstName + ' ' + cli.LastName AS [FullName],
+    a.Country,
+    a.ZIP,
+    CONCAT ('$', 
+    (SELECT MAX(PriceForSingleCigar)
+    FROM Cigars AS cig
+    JOIN ClientsCigars as cc ON cig.Id = cc.CigarId AND cc.ClientId = cli.Id)) AS CigarPrice
+FROM Clients AS cli
+JOIN Addresses AS a ON cli.AddressId = a.Id
+WHERE ISNUMERIC(a.ZIP) = 1
+ORDER BY FullName ASC
+
+--10.	Cigars by Size
+SELECT
+    c.LastName,
+    AVG(sz.Length) AS CiagrLength,
+    CEILING(AVG(sz.RingRange)) AS CiagrRingRange
+FROM Clients c
+JOIN ClientsCigars cc ON c.Id = cc.ClientId
+JOIN Cigars cig On cc.CigarId = cig.Id
+JOIN Sizes sz ON cig.SizeId = sz.Id
+GROUP BY c.LastName
+ORDER BY CiagrLength DESC
