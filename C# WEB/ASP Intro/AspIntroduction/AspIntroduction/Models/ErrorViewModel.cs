34b8c9ba53fe918f8e105d1d4b00cadfe85e0303
Radoslav Bogdanov <radimk8812@abv.bg>
2022-10-26 14:10:09 +0300

    C# WEB

---
+++ C# WEB/ASP Intro/AspIntroduction/AspIntroduction/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AspIntroduction.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
