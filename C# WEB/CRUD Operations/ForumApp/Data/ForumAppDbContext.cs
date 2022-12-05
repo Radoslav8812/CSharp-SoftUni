6b767ead520f94b5d407f25dfcb6f062c14c4b25
Radoslav Bogdanov <radimk8812@abv.bg>
2022-12-05 22:16:49 +0200

    C# Web CRUD

---
+++ C# WEB/CRUD Operations/ForumApp/Data/ForumAppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using ForumApp.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ForumApp.Data
+{
+	public class ForumAppDbContext : DbContext
+	{
+		public ForumAppDbContext(DbContextOptions<ForumAppDbContext> options) : base(options)
+		{
+		}
+
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+            builder.ApplyConfiguration<Post>(new PostConfiguration());
+
+            builder.Entity<Post>()
+                .Property(p => p.IsDeleted)
+                .HasDefaultValue(false);
+            //SeedPosts();
+            //builder.Entity<Post>()
+            //    .HasData(FirstPost, SecondPost, ThirdPost);
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
+
