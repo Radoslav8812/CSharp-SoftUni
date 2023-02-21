16cd9647b5f2d4a9d0a9c87fa98ecebbac6df17b
Radoslav Bogdanov <radimk8812@abv.bg>
2023-02-21 10:26:44 +0200

    C#

---
+++ WHOLE EDUCATION/Basics/Loops/ArrayЕxample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ArrayЕxample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+
+            for (int i = 0; i < n; i++)
+            {
+                var num = i + 1;
+
+                if (num % 2 != 0 || num == 10)
+                {
+                    continue;
+                }
+
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
+
