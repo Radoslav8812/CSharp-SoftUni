62e6c7bae46de577ff21a3a98829de402c221295
Radoslav Bogdanov <radimk8812@abv.bg>
2024-08-19 16:37:30 +0300

    c#

---
+++ C# Adv/ParkingLot/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var input = Console.ReadLine().Split();
+var set = new HashSet<string>();
+
+while (input[0] != "END")
+{
+    var direction = input[0];
+    var number = input[1];
+
+    if (direction == "IN" && !set.Contains(number))
+    {
+        set.Add(number);
+    }
+    else if (direction == "OUT" && set.Contains(number))
+    {
+        set.Remove(number);
+    }
+
+    input = Console.ReadLine().Split();
+}
+
+foreach (var item in set)
+{
+    if (set.Count != 0)
+    {
+        Console.WriteLine($"Number Still Inside: {item}");
+    }
+    else
+    {
+        Console.WriteLine("Our parking is empty.");
+    }
+}
+
